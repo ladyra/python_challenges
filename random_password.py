fcbf895ad862749ce028fcbea5e262d54227a481
Rasha AMIN <ramin@prismamedia.com>
2021-01-24 20:44:20 +0100

    improve password strength

--- random_password.py
+++ random_password.py
@@ -16,13 +16,18 @@ try:
     # open the file for reading
     f = open(filename,"r")
     words = f.read().split()
-    # generate a random password
-    password = random.choice(words) + random.choice(words)
+    while 1:
+        # generate a random password
+        word1 = random.choice(words).title()
+        word2 = random.choice(words).title()
+        password = word1 + word2
+        # check password strength
+        if len(word1) >= 3 and len(word2) >= 3 and 8 < len(password):
+            # print out the generated password
+            print(password)
+            break
     # close the file
     f.close()
 except:
     print("An error occurred while processing the file.")
     quit()
-
-# print out the generated password
-print(password)
